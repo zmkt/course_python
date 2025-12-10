712abc7e537bf82df31922afcf990f10ffefd135
Ilya Kirov <zmkt@mail.ru>
2025-12-10 09:59:51 +0300

    сделал недостающую часть задания

---
+++ 1-vars/expences.py
@@ -0,0 +1,11 @@
+foodStr = input("Введите расходы на еду: ")
+transportationStr = ("Введите расходы на транспорт: ")
+entertainmentStr = ("Введите расходы на развлечения: ")
+
+food = int(foodStr)
+transportation = int(transportationStr)
+entertainment = int(entertainmentStr)
+
+result = food + transportation + entertainment
+
+print(result)
