f041e8650e5a174a694ccb6a4ad573996559c84c
Ilya Kirov <zmkt@mail.ru>
2025-12-10 10:02:32 +0300

    fix code

--- 1-vars/expences.py
+++ 1-vars/expences.py
@@ -1,6 +1,6 @@
 foodStr = input("Введите расходы на еду: ")
-transportationStr = ("Введите расходы на транспорт: ")
-entertainmentStr = ("Введите расходы на развлечения: ")
+transportationStr = input("Введите расходы на транспорт: ")
+entertainmentStr = input("Введите расходы на развлечения: ")
 
 food = int(foodStr)
 transportation = int(transportationStr)
